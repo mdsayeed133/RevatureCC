05d435968e361aaa6db64a1a1ea442daf78fbda5
Mohammed Sayeed Islam <120868662+mdsayeed133@users.noreply.github.com>
2023-03-14 07:51:04 -0700

    done

---
+++ CC/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        minuteToSecond(2);
+    }
+
+
+    public static void minuteToSecond(int minute){
+        System.out.println(minute+" minute is "+minute*60+" seconds");
+    }
+}
