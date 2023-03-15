1917af54a8015a6af0f47811e669c6ad01fe2be2
Mohammed Sayeed Islam <120868662+mdsayeed133@users.noreply.github.com>
2023-03-15 07:23:41 -0700

    done

--- CC/src/main/java/org/example/Main.java
+++ CC/src/main/java/org/example/Main.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        minuteToSecond(5);
+        minuteToSecond(3);
         minuteToSecond(2);
     }
 
